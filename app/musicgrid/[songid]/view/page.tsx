37a4bef32453828c812c56445d7f0627fa599c83
Paulthelol <paullukebetzen@gmail.com>
2025-03-24 03:17:22 -0500

    Add empty returns for empty pages

---
+++ app/musicgrid/[songid]/view/page.tsx
@@ -0,0 +1,6 @@
+export default function ViewSong() {
+
+    return (
+        <main></main>
+    )
+}
---
+++ app/musicgrid/page.tsx
@@ -0,0 +1,6 @@
+export default function MusicGrid() {
+
+    return (
+        <main></main>
+    )
+}
