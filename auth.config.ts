9d9423bc19d2a2f232bf77e43cfcf9b5471d0105
Paulthelol <paullukebetzen@gmail.com>
2025-03-31 12:16:43 -0500

    Start middleware implementation

---
+++ auth.config.ts
@@ -0,0 +1,29 @@
+import type { NextAuthConfig } from "next-auth";
+import Spotify from "next-auth/providers/spotify"
+
+export const authConfig = {
+    providers: [Spotify],
+    pages: {
+      signIn: "/login",
+      error: "/autherror",
+    },
+    callbacks: {
+      authorized({ auth, request: { nextUrl } }) {
+        const isLoggedIn = !!auth?.user;
+        const isOnMusicGrid = nextUrl.pathname.startsWith('/musicgrid');
+        const isOnLoginpage = nextUrl.pathname.startsWith('/login');
+        
+        if (isOnMusicGrid) {
+          return isLoggedIn;
+        } else if (isOnLoginpage) {
+          if (isLoggedIn) {
+            return Response.redirect(new URL('/musicgrid', nextUrl));
+          }
+        }
+        else if (isLoggedIn) {
+          return isLoggedIn;
+        }
+        return true;
+      },
+    },
+} satisfies NextAuthConfig;
