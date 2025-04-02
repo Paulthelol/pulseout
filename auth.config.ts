9f51c0fe91e726c489a157e99ee86d6882ed4aa5
Paulthelol <paullukebetzen@gmail.com>
2025-04-02 12:29:06 -0500

    Modify callback logic

--- auth.config.ts
+++ auth.config.ts
@@ -3,29 +3,27 @@ import Spotify from "next-auth/providers/spotify"
 
 //Responsible for enforcing middleware rules
 export const authConfig = {
-    providers: [Spotify],
-    pages: {
-      signIn: "/login",
-      error: "/autherror",
-    },
-    callbacks: {
-      authorized({ auth, request: { nextUrl } }) {
-        console.log("Middleware run!");
-        const isLoggedIn = !!auth?.user;
-        const isOnMusicGrid = nextUrl.pathname.startsWith('/musicgrid');
-        const isOnLoginpage = nextUrl.pathname.startsWith('/login');
-        
-        if (isOnMusicGrid) {
-          return isLoggedIn;
-        } else if (isOnLoginpage) {
-          if (isLoggedIn) {
-            return Response.redirect(new URL('/musicgrid', nextUrl));
-          }
+  providers: [Spotify],
+  pages: {
+    signIn: "/login",
+    error: "/autherror",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnMusicGrid = nextUrl.pathname.startsWith('/musicgrid');
+      const isOnLoginpage = nextUrl.pathname.startsWith('/login');
+
+      if (isOnMusicGrid) {
+        return isLoggedIn;
+      } else if (isOnLoginpage) {
+        if (isLoggedIn) {
+          return Response.redirect(new URL('/musicgrid', nextUrl));
         }
-        else if (isLoggedIn) {
-          return isLoggedIn;
-        }
-        return true;
-      },
+      } else if (!isLoggedIn) {
+        return isLoggedIn;
+      }
+      return true;
     },
+  },
 } satisfies NextAuthConfig;
