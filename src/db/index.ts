1bc20062ca4dfd856ad2bcfc4acd4cc61088799a
Paulthelol <paullukebetzen@gmail.com>
2025-04-02 19:31:02 -0500

    Authentication Database Configuration

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+export const db = drizzle(sql);
+
+
