82b20dd6c30122429cc89360c4a763712c5e8cef
JordanStec <xxyashidoyukixx@gmail.com>
2025-04-08 04:17:26 -0500

    Update side-bar.tsx

--- app/ui/side-bar.tsx
+++ app/ui/side-bar.tsx
@@ -1,15 +1,38 @@
-import { SignOut } from "@/lib/actions"
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './side-bar.module.css';
 
 export default function SideBar() {
+  return (
+    <aside className={styles.sideBar}>
+      <div className={styles.topSection}>
+        <Image
+          src="/logo.png"
+          alt="PulseOut Logo"
+          width={140}
+          height={40}
+          className={styles.logoImage}
+        />
 
-    return (
-        <div>
-            <form
-            action={SignOut}>
-                <button>
-                    Sign Out
-                </button>
-            </form>
-        </div>
-    )
+        <nav className={styles.navSection}>
+          <Link href="/trending"><span>Trending</span></Link>
+          <Link href="#"><span>Genres</span></Link>
+          <Link href="#"><span>Likes</span></Link>
+        </nav>
+      </div>
+
+      <div className={styles.bottomSection}>
+        <Link href="#" className={styles.bottomLink}>
+          <span>My Profile</span>
+        </Link>
+
+        <a href="https://pulseout.vercel.app/login" className={styles.bottomLink}>
+          Sign Out
+        </a>
+      </div>
+    </aside>
+  );
 }
