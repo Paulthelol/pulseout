c80175f19061cc1dbe05f02617345828f9a98152
JordanStec <xxyashidoyukixx@gmail.com>
2025-04-08 03:46:45 -0500

    Update layout.tsx
    Goes along with the changes to the welcome page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
-import '@/app/ui/global.css'
-import {noto_sans} from '@/app/ui/fonts'
+import './ui/global.css';
+import { noto_sans } from './ui/fonts';
+import ClientLayout from './ui/client-layout'; 
 
 export const metadata = {
   metadataBase: new URL('https://pulseout.vercel.app/'),
   title: 'Welcome to PulseOut',
-  description:
-    'A new social music platform for music lovers!',
-}
+  description: 'A new social music platform for music lovers!',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${noto_sans.className} antialiased`}>{children}</body>
+    <body className={`${noto_sans.className} antialiased`}>
+        <ClientLayout>{children}</ClientLayout>
+      </body>
     </html>
-  )
+  );
 }
