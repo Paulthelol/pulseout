f1d1f458da5b69a0598637656e47228e1987f086
Paulthelol <paullukebetzen@gmail.com>
2025-04-11 03:11:18 -0500

    Add Search Bar

--- app/ui/side-bar.tsx
+++ app/ui/side-bar.tsx
@@ -4,35 +4,42 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './side-bar.module.css';
+import { SignOut } from '@/lib/actions';
 
 export default function SideBar() {
-  return (
-    <aside className={styles.sideBar}>
-      <div className={styles.topSection}>
-        <Image
-          src="/logo.png"
-          alt="PulseOut Logo"
-          width={140}
-          height={40}
-          className={styles.logoImage}
-        />
+    return (
+        <aside className={styles.sideBar}>
+            <div className={styles.topSection}>
+                <Image
+                    src="/logo.png"
+                    alt="PulseOut Logo"
+                    width={140}
+                    height={40}
+                    className={styles.logoImage}
+                />
 
-        <nav className={styles.navSection}>
-          <Link href="/musicgrid/trending"><span>Trending</span></Link>
-          <Link href="#"><span>Genres</span></Link>
-          <Link href="#"><span>Likes</span></Link>
-        </nav>
-      </div>
+                <nav className={styles.navSection}>
+                    <Link href="/musicgrid/trending"><span>Trending</span></Link>
+                    <Link href="#"><span>Genres</span></Link>
+                    <Link href="#"><span>Likes</span></Link>
+                </nav>
+            </div>
 
-      <div className={styles.bottomSection}>
-        <Link href="#" className={styles.bottomLink}>
-          <span>My Profile</span>
-        </Link>
+            <div className={styles.bottomSection}>
+                <Link href="#" className={styles.bottomLink}>
+                    <span>My Profile</span>
+                </Link>
 
-        <a href="https://pulseout.vercel.app/login" className={styles.bottomLink}>
-          Sign Out
-        </a>
-      </div>
-    </aside>
-  );
+                <div>
+                    <form
+                        action={SignOut}>
+                        <button className={styles.bottomLink}>
+                            Sign Out
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </aside>
+    );
 }
--- lib/actions.ts
+++ lib/actions.ts
@@ -26,5 +26,5 @@ export async function authenticateSpotify(
 }
 
 export async function SignOut() {
-    await signOut();
+    await signOut({ redirectTo: '/login' });
 }
