785076f1297ee7cbb468e38c5c18e41dd8f3cf99
Paulthelol <paullukebetzen@gmail.com>
2025-04-11 02:44:30 -0500

    Modify middleware for logo

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export default NextAuth(authConfig).auth;
 //Matcher triggers middleware to run (middleware is implemented in authConfig for this project)
 export const config = {
     matcher: [
-      '/((?!api|_next/static|_next/image|Spotify_logo_without_text.svg|sitemap.xml|robots.txt).+)',
+      '/((?!api|_next/static|_next/image|Spotify_logo_without_text.svg|sitemap.xml|robots.txt|logo.png).+)',
     ],
   };
