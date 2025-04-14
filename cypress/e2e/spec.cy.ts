f2ebfbb1f28156b795763e87732f45956b2aa322
Paulthelol <paullukebetzen@gmail.com>
2025-04-13 19:55:50 -0500

    Change to localhost target

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Website Login', () => {
   it('passes', () => {
-    cy.visit('https://pulseout-zeta.vercel.app')
+    cy.visit('http://localhost:3000/')
     cy.contains('Login').click(); 
     cy.contains('Log in with Spotify').click(); 
 
