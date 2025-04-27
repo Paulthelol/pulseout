32a67e8f128bccec358c747d9043764ddf6d3c91
JordanStec <xxyashidoyukixx@gmail.com>
2025-04-27 10:45:43 -0500

    Changes login back background and box color
    light grey bg and dark grey box

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,19 +4,13 @@ import LoginForm from "../ui/login-form";
 import { Suspense } from "react";
 
 export default function LoginPage() {
-  return (
-    <main
-      className="flex flex-col items-center justify-center min-h-screen w-full"
-      style={{
-        background:
-          "linear-gradient(135deg, #b429f9, #9c43f8, #855df7, #6d77f6, #5591f5, #3eabf4, #26c5f3)",
-      }}
-    >
-      <div className="max-w-[400px] w-full">
-        <Suspense>
-          <LoginForm />
-        </Suspense>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-200">
+            <div className="bg-gray-800 px-10 py-12 rounded-xl shadow-lg max-w-md w-full">
+                <Suspense>
+                    <LoginForm />
+                </Suspense>
+            </div>
+        </main>
+    );
 }
