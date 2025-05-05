a2e5fe9362ab5061de786eb59e2fbd7d2999dd5f
AndrewKN49 <AndrewKN49@gmail.com>
2025-05-04 19:25:43 -0500

    Final_Testing

---
+++ app/pageWelcomePage-copy-3.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import WelcomePage from './page'
+
+describe('<WelcomePage />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<WelcomePage />)
+  })
+})
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  projectId: "j5dpf1",
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
+
